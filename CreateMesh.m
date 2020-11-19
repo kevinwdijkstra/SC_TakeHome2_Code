5bee39f57fa4e7183df2796d62e765b4a7a0bac4
Kevin Dijkstra <kevin.w.dijkstra@gmail.com>
2020-11-19 15:41:30 +0100

    Initial Commit

---
+++ CreateMesh.m
@@ -0,0 +1,16 @@
+function [D2Mesh, D3Mesh, x, y, z] = CreateMesh(n)
+
+    x = linspace(0,1,n+1);
+    y = linspace(0,1,n+1);
+    z = linspace(0,1,n+1);
+    
+    x = x(2:end-1);
+    y = y(2:end-1);
+    z = z(2:end-1);
+    
+    D2Mesh = combvec(x,y);
+    D3Mesh = combvec(D2Mesh,z);
+
+
+end
+
