bd953cd3d3d53acdd12272d6040dd709d412f748
cerbrendus <34724641+cerbrendus@users.noreply.github.com>
2020-11-22 21:00:36 +0100

    Sparsity van matrices geïmplementeerd

--- CreateMatrix.m
+++ CreateMatrix.m
@@ -2,8 +2,8 @@ function [D2Mat, D3Mat] = CreateMatrix(n)
 
     D1Mat = Create1D(n-1);
 
-    D2Mat = (kron(D1Mat,eye(n-1))+kron(eye(n-1),D1Mat));
-    D3Mat = kron(D2Mat,eye(n-1))+kron(eye((n-1)^2),D1Mat);
+    D2Mat = (kron(D1Mat,speye(n-1))+kron(speye(n-1),D1Mat));
+    D3Mat = kron(D2Mat,speye(n-1))+kron(speye((n-1)^2),D1Mat);
     
     D2Mat = (n)^2*D2Mat;
     D3Mat = (n)^3*D3Mat;
@@ -14,6 +14,7 @@ end
 
 
 function [D1Mat] = Create1D(n)
-    D1Mat = (2*eye(n) -diag(ones(n-1,1),1) - diag(ones(n-1,1),-1));
+    e = ones(n,1);
+    D1Mat = spdiags([-e 2*e -e],-1:1,n,n);
 end
 
