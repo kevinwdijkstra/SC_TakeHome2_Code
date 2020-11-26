554b27997a4fdbc6ecdb69291f0a438dcd487206
cerbrendus <34724641+cerbrendus@users.noreply.github.com>
2020-11-26 22:28:02 +0100

    Variabelnamen gestandaardiseerd

--- CholeskyDecompostion.m
+++ CholeskyDecompostion.m
@@ -4,10 +4,10 @@ function [C] = CholeskyDecompostion(A)
 
     [n,~] = size(A);
 %     [i,j,a] = find(A);
-    [N,M] = bandwidth(A);
+    [p,~] = bandwidth(A);
     
     C = 0*A;
-    for k = 1:n
+    for j = 1:n
 %         Ck     = sqrt(A(k,k)-sum(C(k,max((k-N),1):k-1).^2));
 %         C(k,k) = Ck;
 %         A(k,k) = Ck;
@@ -21,22 +21,22 @@ function [C] = CholeskyDecompostion(A)
 %             A(i,k) = C(i,k);
 %         end
         %% vectored
-        Id1 = k+1:min(k+M,n);       % second for loop over i
-        Id2 = max((k-N),1):(k-1);   % both sums over j
+        Id1 = j+1:min(j+p,n);       % second for loop over i
+        Id2 = max((j-p),1):(j-1);   % both sums over j
         
         % extract data in vector and matrix data so that all points over i
         % can be calculated at once (vectorized code for speed). This turns
         % the sum to a matrix vector product.
-        Vec = C(k,Id2);
+        Vec = C(j,Id2);
         Mat = C(Id1,Id2);
         
         % Note the norm is just a matrix vector product, however the norm
         % makes more clever use of the sparse vector for speed up.
-        Ck     = sqrt(A(k,k)-norm(Vec)^2);
-        C(k,k) = Ck; %store C(k,k) as accessing data takes some time.
+        Cj     = sqrt(A(j,j)-sum((Vec).^2));
+        C(j,j) = Cj; %store C(k,k) as accessing data takes some time.
         
         % calculate the entire second for loop over i in one go.
-        C(Id1,k) = 1/Ck*(A(Id1,k) - Mat*Vec');
+        C(Id1,j) = 1/Cj*(A(Id1,j) - Mat*Vec');
     end
 
 end
