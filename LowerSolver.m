b7e8d1bbe0dde504a2212bf2d42230e00e840c91
Kevin Dijkstra <kevin.w.dijkstra@gmail.com>
2020-11-26 17:54:17 +0100

    added comments

--- LowerSolver.m
+++ LowerSolver.m
@@ -1,18 +1,19 @@
 function [x] = LowerSolver(C,y)
 %% solve Cx = y when C is a lower triangle matrix,
-
+% find size of matrix C and pre-alocate memory
 [~,m] = size(C);
 x = zeros(1,m);
 
-
-% [I,J,Val] = find(C);
+% loop over x from first value to last.
 for i = 1:m
-%     Id  = (I==i);
-%     j   = J(Id);
-%     S   = Val(Id)';
-    
+    % get the ith row from C
     SubC = C(i,:);
+    
+    % get the y index and value of the ith row of C
     [~,j,S] = find(SubC);
+    
+    % calculate new C. This is x(i) = (y(i) - sum_j=1^i-1 C(i,j)x(j))/C(i,i)
+    % By indexing only the necesary values we get a speed up
     x(i) = (y(i) - sum(S(1:end-1).*x(j(1:end-1))))/S(end);
 end
 
--- UpperSolver.m
+++ UpperSolver.m
@@ -1,12 +1,19 @@
 function [x] = UpperSolver(C,y)
 %% solve Cx = y when C is a upper triangle matrix,
-
+% find size of matrix C and pre-alocate memory
 [~,m] = size(C);
 x = zeros(1,m);
 
+% loop over x from last value to first.
 for i = m:-1:1
+    % get the ith row from C
     SubC = C(i,:);
+    
+    % get the y index and value of the ith row of C
     [~,j,S] = find(SubC);
+    
+    % calculate new C. This is x(i) = (y(i) - sum_j=1^i-1 C(i,j)x(j))/C(i,i)
+    % By indexing only the necesary values we get a speed up
     x(i) = (y(i) - sum(S(2:end).*x(j(2:end))))/S(1);
 end
 
