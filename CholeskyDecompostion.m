db289fdc899fde7d6f9134587c2b22f831a11b97
cerbrendus <34724641+cerbrendus@users.noreply.github.com>
2021-01-15 11:39:18 +0100

    Revert "merge"
    This reverts commit e15cdb9a0f917b03a940db26283ee6875c3ddfd0, reversing
    changes made to ab165b514650ed6f9dec34c3e73a54b04d893dd2.

--- CholeskyDecompostion.m
+++ CholeskyDecompostion.m
@@ -1,5 +1,4 @@
 function [A] = CholeskyDecompostion(A)
-<<<<<<< HEAD
 %% calculate the Cholesky Decomposition
 % inputs:
 % - A the matrix
@@ -45,34 +44,6 @@ for j = 1:n % loop over  each row
     A(Id1,j) = 1/Cj*(A(Id1,j) - Mat*Vec');
 end
 A = tril(A);
-=======
-% CHOLESYDECOMPOSITION Compute the Cholesky factor for matrix A according
-% to Algorithm 1 from the lecture notes.
-%   A = CHOLESKYDECOMPOSITION(A)  
-
-    [n,~] = size(A); % Compute matrix dimensions n
-    [p,~] = bandwidth(A); % Compute bandwidth of A
-    for j = 1:n
-        %% vectored
-        Id1 = j+1:min(j+p,n);       % second for loop over i
-        Id2 = max((j-p),1):(j-1);   % both sums over j
-        
-        % extract data in vector and matrix data so that all points over i
-        % can be calculated at once (vectorized code for speed). This turns
-        % the sum to a matrix vector product.
-        Vec = A(j,Id2);
-        Mat = A(Id1,Id2);
-        
-        % Note the norm is just a matrix vector product, however the norm
-        % makes more clever use of the sparse vector for speed up.
-        Cj     = sqrt(A(j,j)-norm(Vec)^2);
-        A(j,j) = Cj; %store C(k,k) as accessing data takes some time.
-        
-        % calculate the entire second for loop over i in one go.
-        A(Id1,j) = 1/Cj*(A(Id1,j) - Mat*Vec');
-    end
-    A = tril(A); % Extract lower triangular part of matrix
->>>>>>> comments
 
 end
 
--- Direct_Solve.m
+++ Direct_Solve.m
@@ -1,38 +1,34 @@
 function [u,times_fac,times_sol,NNZ] = Direct_Solve(A,f,solve_options,n,dim)
-% DIRECT_SOLVE Solve the equation Au=f directly using the Cholesky
-% decomposition.
-%   [u,TIMES_FAC,TIMES_SOL,NNZ] = DIRECT_SOLVE(A,F,SOLVE_OPTIONS,N,DIM)
-%   solves AU=F using Cholesky decomposition. It returns a the duration of
-%   factorization TIMES_FAC, the duration of solving TIMES_SOL, and the
-%   number of nonzero elements NNZ of matrix A and the cholesky factor.
 
-    NNZ = zeros(1,2); % Initialize variable
-    NNZ(1) = nnz(A); % Compute the number of nonzero elements of A
-    
-    % Compute the Cholesky factor of A
-    if solve_options.use_MATLAB % Use MATLAB's implementation
+    NNZ = zeros(1,2);
+    NNZ(1) = nnz(A);
+    if solve_options.use_MATLAB
         tic;
         C = chol(A,'lower');
         t_end = toc;
-    else % Use our implementation
+    else
         tic
         C = CholeskyDecompostion(A);
         t_end = toc;
     end
-    times_fac = t_end; % Save the duration of factorization
+    times_fac = t_end;
         
-    NNZ(2) = nnz(C); % Compute the number of nonzero elements of C
+   
+    NNZ(2) = nnz(C);
 
-    % Solve the matrix equation
-    if solve_options.use_MATLAB % Use MATLAB's implementation
+    % solve 2D problem
+    if solve_options.use_MATLAB
         tic;
         u = C'\(C\(f));
         t_end = toc;
-    else % Use our implementation
+    else
         tic;
         u = UpperSolver(C',LowerSolver(C,f,(n-1)^(dim-1)),(n-1)^(dim-1));
         t_end = toc;
     end
-    times_sol = t_end; % Save the duration of solving
+    times_sol = t_end;
+    % reverse reordering
+    
+
 end
 
